70c023a689952eef7b30a6f098fcff8370d792d0
Colin Humber <colinhumber@gmail.com>
2022-01-28 09:45:18 -0700

    Version 0.1.0

--- Package.swift
+++ Package.swift
@@ -8,14 +8,14 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "DangerSwiftJiraIssue", targets: ["DangerSwiftJiraIssue"]),
-        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]) // dev
+//        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]) // dev
     ],
     dependencies: [
         .package(url: "https://github.com/danger/swift", from: "3.0.0"),
-        .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev,
+//        .package(url: "https://github.com/f-meloni/Rocket", from: "1.0.0"), // dev,
     ],
     targets: [
-        .target(name: "DangerDependencies", dependencies: [.product(name: "Danger", package: "swift"), "DangerSwiftJiraIssue"]), // dev
+//        .target(name: "DangerDependencies", dependencies: [.product(name: "Danger", package: "swift"), "DangerSwiftJiraIssue"]), // dev
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
